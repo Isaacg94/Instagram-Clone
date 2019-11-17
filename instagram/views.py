1fae56fd63bc3622ac0fc1d9c4f76ddaa3b92e75
Isaacg94 <7248zack@gmail.com>
2019-11-17 21:59:24 +0300

    add first template

--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
