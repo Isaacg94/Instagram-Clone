225d093e634981f03e3fdb9f7956a8adb31b9609
Isaacg94 <7248zack@gmail.com>
2019-11-18 15:23:23 +0300

    add login decorator before feed view function to limit access to the timeline

--- instagram/views.py
+++ instagram/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 from .models import Image
+from django.contrib.auth.decorators import login_required.
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
+@login_required(login_url='/accounts/login/')
 def feed(request):
     images = Image.newsfeed()
     return render(request, 'timeline.html', {"images":images})
