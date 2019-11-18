485b6da112e2e045f2a99ed7665dab7c39a8cd0a
Isaacg94 <7248zack@gmail.com>
2019-11-18 10:10:48 +0300

    add Image model to handle image data from the db

--- instagram/models.py
+++ instagram/models.py
@@ -6,3 +6,9 @@ from pyuploadcare.dj.models import ImageField
 class Profile(models.Model):
     bio = models.TextField()
     profile_pic = ImageField( blank = True)
+
+class Image(models.Model):
+    img = models.ImageField(upload_to='feed/')
+    img_name = models.CharField(max_length= 30)
+    img_caption = models.TextField()
+    profile = models.ForeignKey(Profile)
