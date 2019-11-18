4e97bffb2957b801b65be2abd10f8781748c9223
Isaacg94 <7248zack@gmail.com>
2019-11-18 10:08:33 +0300

    add profile model

--- instagram/models.py
+++ instagram/models.py
@@ -2,3 +2,7 @@ from django.db import models
 from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
+
+class Profile(models.Model):
+    bio = models.TextField()
+    profile_pic = ImageField( blank = True)
