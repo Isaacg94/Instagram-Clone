c1122504fa14fe0e55d12ce3a44dda1f83bda15e
Isaacg94 <7248zack@gmail.com>
2019-11-18 11:17:33 +0300

    add view function to display queried images

--- instagram/views.py
+++ instagram/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
+from .models import Image
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
 def feed(request):
-    return render(request, 'timeline.html')
+    images = Image.newsfeed()
+    return render(request, 'timeline.html', {"images":images})
